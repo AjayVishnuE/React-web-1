983f66705b991132785661808d52440b74ae36dd
AjayVishnuE <ajayvishnu121@gmail.com>
2023-05-09 13:55:28 +0530

    Update 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { ReactDOM } from 'react-dom';
 
+import { Footer, Blog, Possibility, Features, WhaGPT3, Header } from './containers' ;
+import { CTA, Brand, Navbar } from './components' ;
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello There</h1>
+    <div className='App'>
+      <div className='gradient-bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhaGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
